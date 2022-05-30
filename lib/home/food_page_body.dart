c9ceb28d6638af69c4d8dcdb543e4230d9feb8ca
HZHCoder1990 <2282802627@qq.com>
2022-05-31 00:30:20 +0800

    更新

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -8,25 +8,49 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+
+  PageController pageController = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 320,
-      child: PageView.builder(
-        itemCount: 5,
-        itemBuilder: (context, position) {
-        return _buildPageItem(position);
-      })
-    );
+        color: Colors.red,
+        height: 320,
+        child: PageView.builder(
+            controller: pageController,
+            itemCount: 5,
+            itemBuilder: (context, position) {
+              return _buildPageItem(position);
+            }));
   }
 
+  // Container内嵌Container时，内部的Container的size默认和外部的size一样。
+  // 为了可以单独设置内部Container的size，可以使用Stack把内部的Container包裹起来
+
   Widget _buildPageItem(int position) {
-    return Container(
-      margin: const EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color:const Color(0xFF69C5DF)
-      ),
+    return Stack(
+      children: [
+        Container(
+          height: 250,
+          margin: const EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: const Color(0xFF69C5DF),
+              image: const DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/image/food0.png'))),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white
+            ),
+          ),
+        )
+      ],
     );
   }
 }
